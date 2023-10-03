f2dda49af63c0ce1567295eb7a9e469a0977596e
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-10-03 15:02:37 +0100

    complete problemset2b

--- ProblemSets/src/main/java/ProblemSet_2b/Meal.java
+++ ProblemSets/src/main/java/ProblemSet_2b/Meal.java
@@ -1,5 +1,18 @@
 package ProblemSet_2b;
 
 public class Meal {
-
+	private Food mainCourse;
+	private Food dessert;
+	public Meal(Food mainCourse, Food dessert) {
+		this.dessert = dessert;
+		this.mainCourse = mainCourse;
+	}
+    	public double calculateTotalSugarLevel() {
+    		return this.dessert.getAmountOfSugar() + this.mainCourse.getAmountOfSugar();
+    	}
+    	
+    	public String toString () {
+    		return "Meal [mainCourse = " + this.mainCourse.getName()
+    		+ ", dessert = " + this.dessert.getName() + "]"; 		
+    	}
 }
