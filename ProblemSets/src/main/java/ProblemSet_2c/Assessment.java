c8f8b1b3c437be9bc73dcd0fc385f7c6b0337027
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-10-04 13:59:07 +0100

    completedProblemSet_2c

---
+++ ProblemSets/src/main/java/ProblemSet_2c/Assessment.java
@@ -0,0 +1,16 @@
+package ProblemSet_2c;
+
+public class Assessment {
+	private String name;
+	private double mark;
+	public Assessment(String name, double mark) {
+		this.name = name;
+		this.mark = mark;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getMark() {
+		return mark;
+	}
+}
