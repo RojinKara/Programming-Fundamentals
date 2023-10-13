7adbf036654eb72cecdd55c40b082e5a9f5ee612
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-10-13 12:58:19 +0100

    completedProblemSet_3b

---
+++ ProblemSets/src/main/java/ProblemSet_3b/AnnualSalary.java
@@ -0,0 +1,29 @@
+package ProblemSet_3b;
+
+public class AnnualSalary {
+	private double salary;
+	private final double PERSONAL_ALLOWANCE = 12570;
+	public AnnualSalary() { }
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double calculateTax() {
+		double tax = 0;
+		double copy = salary;
+		if (copy > 125140) {
+			tax += (copy - 125140) * 0.45;
+			copy = 125140;
+		}
+		if (copy > 50270) {
+			tax += (copy - 50270) * 0.40;
+			copy = 50270;
+		}
+		if (copy > PERSONAL_ALLOWANCE) {
+			tax += (copy - PERSONAL_ALLOWANCE) * 0.20;
+		}
+		return tax;
+	}
+}
