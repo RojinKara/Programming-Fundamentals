7cd2fdf17602ebd51df273db88e74f842b0f16df
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-10-17 23:01:02 +0100

    completedProblemSet-4a

---
+++ ProblemSets/src/main/java/ProblemSet_4a/Day.java
@@ -0,0 +1,33 @@
+package ProblemSet_4a;
+
+public class Day {
+	private String name;
+	private Session[] sessions;
+	public Day(String name) {
+		this.name = name;
+		this.sessions = new Session[4];
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setSession(int index, String moduleName,
+			int startTime, int endTime) {
+		this.sessions[index] =
+				new Session(moduleName, startTime, endTime);
+	}
+	public Session getSessions(int index) {
+		return this.sessions[index];
+	}
+	@Override
+	public String toString() {
+		String temp = "";
+		for (int i = 0; i < this.sessions.length; i++) {
+			if (this.sessions[i] != null) {
+				temp += this.sessions[i].getSessionName() + ": "
+					+ this.sessions[i].getStartTime() + " - "
+					+ this.sessions[i].getEndTime() + "\n";
+			}
+		}
+		return temp;
+	}
+}
