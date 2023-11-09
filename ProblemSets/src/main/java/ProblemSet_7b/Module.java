70108f451ab09eb6080b76ba367807839b8d932c
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-11-09 15:47:08 +0000

    completed problemset 7b

---
+++ ProblemSets/src/main/java/ProblemSet_7b/Module.java
@@ -0,0 +1,15 @@
+package ProblemSet_7b;
+
+public class Module {
+	private String name = null;
+	public Module(String name) {
+		if (name.matches("COM\\d{4}")) {
+			this.name = name;
+		} else {
+			this.name = "Error";
+		}
+	}
+	public String getName() {
+		return this.name;
+	}
+}
